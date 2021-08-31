f6d15ebf70f7482806f6db2219c30cd4d9863750
Calvin Dempsey Jr <calvinsy@ad.unc.edu>
2021-08-31 00:08:38 -0400

    FINALLY DONNNEEEEEE

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -2,11 +2,11 @@
 
 __author__ = "730482431"
 
-Nummer_Ein: int = 7
-Nummer_Zwei: int = 5
-print("Left-Hand_side: " + "7")
-print("Right-Hand_side: " + "5")
-print("7 ** 5 == 16807")
-print("7 / 5 is == 1.4")
-print("7 // 5 is 1")
-print("7 % 5 is 2")
+x: str = input("Left-Hand_side: ")
+y: str = input("Right-Hand_side: ")
+x1: int = int(x)
+y1: int = int(y)
+print(x1**y1)
+print(x1/y1)
+print(x1//y1)
+print(x1%y1)
