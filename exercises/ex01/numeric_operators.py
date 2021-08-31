6b71275c678616416a6ae926b5cae29e3f3736f3
Calvin Dempsey Jr <calvinsy@ad.unc.edu>
2021-08-31 10:27:08 -0400

    Even More GradeScope Bug Crunching for Ex01

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -2,11 +2,11 @@
 
 __author__ = "730482431"
 
-der_nummer_str: str = input("Left-Hand_side: ")
-der_nummer_str_zwei: str = input("Right-Hand_side: ")
+der_nummer_str: str = input("Left-hand_side: ")
+der_nummer_str_zwei: str = input("Right-hand_side: ")
 der_nummer_int: int = int(der_nummer_str)
 der_nummer_int_zwei: int = int(der_nummer_str_zwei)
-print(der_nummer_int ** der_nummer_int_zwei)
-print(der_nummer_int / der_nummer_int_zwei)
-print(der_nummer_int // der_nummer_int_zwei)
-print(der_nummer_int % der_nummer_int_zwei)
+print(der_nummer_str, "** " + der_nummer_str_zwei, "is", der_nummer_int ** der_nummer_int_zwei)
+print(der_nummer_str, "/ " + der_nummer_str_zwei, "is", der_nummer_int / der_nummer_int_zwei)
+print(der_nummer_str, "// " + der_nummer_str_zwei, "is", der_nummer_int // der_nummer_int_zwei)
+print(der_nummer_str, "% " + der_nummer_str_zwei, "is", der_nummer_int % der_nummer_int_zwei)
--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -2,11 +2,11 @@
 
 __author__ = "730482431"
 
-nummer_ein_str: str = input("Left-Hand_side: ")
-nummer_zwei_str: str = input("Right-Hand_side: ")
+nummer_ein_str: str = input("Left-hand_side: ")
+nummer_zwei_str: str = input("Right-hand_side: ")
 nummer_ein_int: int = int(nummer_ein_str)
 nummer_zwei_int: int = int(nummer_zwei_str)
-print(nummer_ein_int < nummer_zwei_int)
-print(nummer_ein_int >= nummer_zwei_int)
-print(nummer_ein_int == nummer_zwei_int)
-print(nummer_ein_int != nummer_zwei_int)
+print(nummer_ein_str, "< " + nummer_zwei_str, "is", nummer_ein_int < nummer_zwei_int)
+print(nummer_ein_str, ">= " + nummer_zwei_str, "is", nummer_ein_int >= nummer_zwei_int)
+print(nummer_ein_str, "== " + nummer_zwei_str, "is", nummer_ein_int == nummer_zwei_int)
+print(nummer_ein_str, "!= " + nummer_zwei_str, "is", nummer_ein_int != nummer_zwei_int)
