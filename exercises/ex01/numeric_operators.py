5558f48c8e395d5432a38ef0b760bd034d2378a1
Calvin Dempsey Jr <calvinsy@ad.unc.edu>
2021-08-30 22:21:58 -0400

    Final Commit With Print Functions

---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,12 @@
+"""Exercise 01 For Comp 110 fall 2021: Numeric Operators Assignment."""
+
+__author__ = "730482431"
+
+Nummer_Ein: int = 7
+Nummer_Zwei: int = 5
+print("Left-Hand_side: " + "7")
+print("Right-Hand_side: " + "5")
+print("7 ** 5 == 16807")
+print("7 / 5 is == 1.4")
+print("7 // 5 is 1")
+print("7 % 5 is 2")
