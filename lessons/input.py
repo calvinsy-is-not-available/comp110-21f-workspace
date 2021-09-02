c328e90f6f4e82c05b0777ff4a151183050a3a4d
Calvin Dempsey Jr <calvinsy@ad.unc.edu>
2021-09-02 11:59:48 -0400

    Reverting back to normal after testing

--- lessons/input.py
+++ lessons/input.py
@@ -2,13 +2,3 @@
 
 print("Wow, " + input("Who are you? ") + ", you rock!")
 print(input("Who are you? ") + " have a great day!")
-
-print(der_nummer_str, "** " + der_nummer_str_zwei, " is", der_nummer_int ** der_nummer_int_zwei)
-print(der_nummer_str, "/ " + der_nummer_str_zwei, " is", der_nummer_int / der_nummer_int_zwei)
-print(der_nummer_str, "// " + der_nummer_str_zwei, " is", der_nummer_int // der_nummer_int_zwei)
-print(der_nummer_str, "% " + der_nummer_str_zwei, " is", der_nummer_int % der_nummer_int_zwei)
-
-print(nummer_ein_str, "< " + nummer_zwei_str, " is", nummer_ein_int < nummer_zwei_int)
-print(nummer_ein_str, ">= " + nummer_zwei_str, " is", nummer_ein_int >= nummer_zwei_int)
-print(nummer_ein_str, "== " + nummer_zwei_str, " is", nummer_ein_int == nummer_zwei_int)
-print(nummer_ein_str, "!= " + nummer_zwei_str, " is", nummer_ein_int != nummer_zwei_int)
