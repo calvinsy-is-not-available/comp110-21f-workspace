3e4c086882f93211a2106cd95c1380c754b884eb
Calvin Dempsey Jr <calvinsy@ad.unc.edu>
2021-09-05 15:32:24 -0400

    Pre Bug Crunched Finished Versions

--- exercises/ex02/fortune_cookie.py
+++ exercises/ex02/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730482431"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -10,6 +10,19 @@ __author__ = "ENTER YOUR 9-DIGIT PID HERE"
 # For example, consider the function call expression: randint(1, 100)
 # It will evaluate to an int value >= 1 and <= 100. 
 from random import randint
-
-
-# Begin your solution here...
+print("Your fortune cookie says... ")
+key = int 
+key = randint(1, 4)
+wizzard_blizzard = int
+wizzard_blizzard = randint(10, 15)
+if key == 2: 
+    print("Peace will guide your heart to a life where the sequence of your dreams will know no bounds to joy.")
+else:
+    if key == 3:
+        print("You shall see the world through roses and feel life through endless waves of love.")
+    if key == 1:
+        print("Following a dream, New Years' light will bless you every morning.")
+    if key == 4:
+        print("You will change the world through your conviction of kindness and camaraderie.")
+print("Now, go forth and spread positive vibes!")
+ 
--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -1,6 +1,14 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730482431"
 
 
-# Begin your solution here...
+reeee: str = input("What beat do you want to repeat? ")
+force_of_a_thousand_suns = reeee + " "
+i: int = 0
+maximum: int = int(input("How many times do you want to repeat it? "))
+while i < maximum:
+    i = i + 1
+    print(force_of_a_thousand_suns * (i - 1) + reeee)
+if maximum < 0:
+    print("No beat...")
