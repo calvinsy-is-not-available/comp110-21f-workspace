7255c324308b868a93f5c251c67673749f465701
Calvin Dempsey Jr <calvinsy@ad.unc.edu>
2021-09-05 19:03:51 -0400

    pre bug crunch finished counting letters assing

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -1,6 +1,16 @@
 """Counting letters in a string."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730482431"
 
 
-# Begin your solution here...
+your_letter: str = input("what is your letter?: ")
+your_word: str = input("what is your word: ")
+counter: int = 0 
+maximum = int(your_letter in your_word)
+while counter <= maximum:
+    counter = counter + 1
+    if maximum == 0:
+        print("Count: 0")
+    else:
+        if counter > maximum:
+            print("Count:", + counter)
