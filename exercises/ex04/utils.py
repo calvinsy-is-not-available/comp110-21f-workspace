9948cf9dd91cc7c6173aee6ff732f57615d6b758
Calvin Dempsey Jr <calvinsy@ad.unc.edu>
2021-09-28 20:03:27 -0400

    Done I think

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,58 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730482431"
 
 
-# TODO: Implement your functions here.
+a: bool
+b: bool
+
+
+def all(x: list[int], y: int) -> bool:
+    """Returns a bool: True if all ints are the same."""
+    i: int = 0
+    global a
+    while i < len(x):
+        if x[i] == y:
+            a = True
+        else:
+            a = False
+            return a
+        i += 1
+    return a
+
+
+def is_equal(x: list[int], y: list[int]) -> bool:
+    """Determines if the indicies of two lists are equivalent."""
+    i: int = 0
+    global b
+    while i < len(x):
+        if x[i] == y[i]:
+            b = True
+        else:
+            b = False
+            return b
+        i += 1
+    return b
+
+
+def max(x: list[int]) -> int:
+    """Returns the greatest value from a list of ints."""
+    if len(x) == 0:
+        raise ValueError("max() arg is an empty List")
+    i: int = 0
+    m_a_x: int = 0
+    num: int = x[0]
+    while i < len(x):
+        j: int = i - 1
+        num_2: int = x[j]
+        if x[0] < num:
+            m_a_x = num
+        else:
+            if x[0] > num or x[0] == num:
+                m_a_x = x[0]
+            elif num_2 > num:
+                m_a_x = num_2
+            m_a_x = x[0]
+        i += 1
+        num = x[0 + i - 1]
+    return m_a_x
