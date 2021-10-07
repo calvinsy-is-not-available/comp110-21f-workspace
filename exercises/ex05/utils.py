e25caa53ec9d2e01fa2987c553d390c057688dbd
Calvin Dempsey Jr <calvinsy@ad.unc.edu>
2021-10-07 13:05:04 -0400

    Finished assignment

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,11 +2,11 @@
 
 __author__ = "123456789"
 
-# Define your functions below
-
 
 def only_evens(x: list[int]) -> list[int]:
     """Returns list (if applicable) of even ints."""
+    if x == []:
+        return []
     i: int = 0
     xs: list[int] = []
     while i < len(x):
@@ -17,25 +17,30 @@ def only_evens(x: list[int]) -> list[int]:
 
 
 def sub(x: list[int], y: int, z: int) -> list[int]:
-    """Creates the subset of an Index"""
-    xs: list[int] = []
-    zs: int = len(x) - 1
-    if not y > 0:
-        y = 1
-    if not z < zs:
-        z = len(x)
-    if xs == []:
+    """Creates the subset of an Index."""
+    zs: int = len(x)
+    if len(x) == 0:
         return []
-    if y > len(x):
+    if y == z:
+        return []
+    if y < 0:
+        y = 0
+    if z > zs:
+        z = zs
+    if y == zs or y > zs:
+        return []
+    if z == 0:
         return []
     if z < 0:
         return []
-    xs = [x[y - 1], x[z - 1]]
+    xs = [x[y], x[z - 1]]
     return xs
 
 
 def concat(x: list[int], y: list[int]) -> list[int]:
-    """"Ties to Strings Together."""
+    """Ties to Strings Together."""
+    if x == [] and y == []:
+        return []
     i: int = 0
     while i < len(y):
         x.append(y[i])
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,70 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+
+from exercises.ex05.utils import only_evens, sub, concat
 
 __author__ = "123456789"
+
+
+def test_only_evens_empty_list() -> None:
+    """Test for only evens."""
+    xs: list[int] = []
+    assert only_evens(xs) == []
+
+
+def test_only_evens_increasing() -> None:
+    """Test for only evens."""
+    xs: list[int] = [1, 2, 3, 4, 5, 6, 7]
+    assert only_evens(xs) == [2, 4, 6]
+
+
+def test_only_evens_irratic() -> None:
+    """Test for only evens."""
+    xs: list[int] = [2, 5, 4, 100, 27, 28]
+    assert only_evens(xs) == [2, 4, 100, 28]
+
+
+def test_sub_empty_list() -> None:
+    """Test for sub."""
+    xs: list[int] = []
+    ys: int = 0
+    z: int = 1
+    assert sub(xs, ys, z) == []
+
+
+def test_sub_increasing() -> None:
+    """Test for sub.""" 
+    xs: list[int] = [10, 20, 30, 40, 50, 60, 70, 80]
+    ys: int = 1
+    z: int = 6
+    assert sub(xs, ys, z) == [10, 60]
+
+
+def test_sub_irratic() -> None:
+    """Test for sub."""
+    xs: list[int] = [20, 50, 10, 60, 2100, 50]
+    ys: int = 4
+    z: int = 7
+    assert sub(xs, ys, z) == [60, 50]
+
+
+def test_concat_empty() -> None:
+    """Test for concat."""
+    xs: list[int] = []
+    ys: list[int] = []
+    assert concat(xs, ys) == []
+
+
+def test_concat_if_one_list_is_empty() -> None:
+    """Test for concat."""
+    xs: list[int] = []
+    ys: list[int] = [1, 2, 3, 4, 5, 6, 7]
+    assert concat(xs, ys) == [1, 2, 3, 4, 5, 6, 7]
+
+
+def test_concat_if_both_lists_are_equal() -> None:
+    """Test for concat."""
+    xs: list[int] = [1, 2, 3]
+    ys: list[int] = [1, 2, 3]
+    assert concat(xs, ys) == [1, 2, 3, 1, 2, 3]
