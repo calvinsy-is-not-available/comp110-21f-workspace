b2052f81d66c4880f5a00f410fdc50a2f738de9a
Calvin Dempsey Jr <calvinsy@ad.unc.edu>
2021-11-09 11:20:19 -0500

    class

--- lessons/pointy.py
+++ lessons/pointy.py
@@ -11,22 +11,32 @@ class Point:
         self.x = x
         self.y = y
 
-    def scale_by(self, factor: float) -> None:
-        """Mutates: multiplies components by factor."""
-        self.x *= factor
-        self.y *= factor
+    def __repr__(self) -> str:
+        """Produce a str representation of a Point in Python!"""
+        return f"Point({self.x}, {self.y})"
+    
+    def __mul__(self, factor: float) -> Point:
+        """Overload the multiplication operator for a point multiplied by a float."""
+        return Point(self.x * factor, self.y * factor)
 
-    def scale(self, factor: float) -> Point:
-        """Pure method that does not mutate the Point."""
-        x: float = self.x * factor
-        y: float = self.y * factor
-        scaled_point: Point = Point(x, y)
-        return scaled_point
+    def __add__(self, rhs: Point) -> Point:
+        return Point(self.x + rhs.x, self.y + rhs.y)
+
+    def __getitem__(self, index: int) -> float:
+        """Overload the subscription notation."""
+        if index == 0:
+            return self.x 
+        elif index == 1:
+            return self.y
+        else:
+            raise IndexError
 
 
-p0: Point = Point(1.0, 2.0)
-p0.scale_by(2.0)
-print(f"{p0.x}, {p0.y}")
-
-p1: Point = p0.scale(2.0)
-print(f"{p1.x}, {p1.y}")
+a: Point = Point(1.0, 2.0)
+b: Point = a * 2.0
+c: Point = a + b
+print(f"a: {a}")
+print(f"b: {b}")
+print(f"c: {c}")
+print(a[0])
+print(a[1])
